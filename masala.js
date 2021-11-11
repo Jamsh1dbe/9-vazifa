368e7c85d6e85f1a72fee787fe38c1b153981819
jamsh1dbe <jamshidbeksharipov2@gmail.com>
2021-11-11 15:47:11 +0500

    vazifa10

---
+++ masala.js
@@ -0,0 +1,76 @@
+// raqamlar yigindisini topish
+
+// a=0
+// for (let i = 0; i <10 ; i++) {
+//     a+=i
+// }
+// console.log(a);
+
+//juft sonlar yigindisi
+// a=0
+// for (let i = 10; i <=90; i+=2) {
+//     a+=i
+// }
+// console.log(a);
+
+
+
+// const qosh=(a)=>{
+//     return a+1
+// }
+// console.log(qosh(2));
+
+
+
+// for (let i = 0; i < ar.length; i++) {
+    // }
+    // var arr = [2,3,2,4,6,2,3,5]
+// let i = 0
+// var some = 0
+// var foo = 0
+// sortedArray = arr.sort()
+// while(i < arr.length){
+
+// }
+// console.log(some);
+    ///////////////////////////////////////////////
+
+
+
+
+
+
+
+    ar= [2,3,2,4,6,2,3,5];
+    ar.sort()
+    kk=ar.pop()
+    jk=ar.pop()
+    jj=kk-jk
+    ar.map((value)=>{
+        for (let i = 0; i < ar.length; i++) {
+            // jj>=1&& ar.unshift(jj)
+            // jj=0&& ar===ar
+            console.log(i);
+            
+        }
+    })
+    console.log(ar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ yangimasala.js
@@ -0,0 +1,71 @@
+
+
+
+
+ar= [2,3,2,4,6,2,3,5];
+ar.sort()
+kk=ar.pop()
+jk=ar.pop()
+jj=kk-jk
+for (let i = 0; i <= ar.length; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        jj>=1 && ar.unshift(jj)
+        jj=0 && ar===ar
+    }
+    var arr =ar
+}
+console.log(arr,'=1');
+var a=arr
+ls=arr.pop()
+lsa=arr.pop()
+lsl=(ls-lsa)
+for (let i = 0; i <= a.length; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        lsl>=1 && a.unshift(lsl)
+        lsl=0 && a===a
+    }
+}
+console.log(a,'=2');
+var ab=a
+erl=a.pop()
+re=a.pop()
+l=(erl-re)
+for (let i = 0; i <= ab.length; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        l>=1 && ab.unshift(l)
+        l=0 && ab===ab
+    }
+}
+console.log(ab,'=3');
+var s=ab
+see=ab.pop()
+seen=ab.pop()
+eye=(see-seen)
+for (let i = 0; i <= s.length; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        eye>=1 && s.unshift(eye)
+        eye=0 && s===s
+    }
+}
+console.log(s,'=4');
+var d=s
+seed=d.pop()
+seend=d.pop()
+eyed=(seed-seend)
+for (let i = 0; i <= d.length; i++) {
+    
+    for (let j = 0; j < i; j++) {
+        eyed>=1 && d.unshift(eyed)
+        eyed=0 && d===d
+    }
+}
+console.log(d,'=5');
+
+
+/////////////////////////////////////////
+
+

258cc12c64c12e07891e872817c9fdcc24a95864
jamsh1dbe <jamshidbeksharipov2@gmail.com>
2021-11-15 23:13:13 +0500

    test

---
+++ savollar.js
@@ -0,0 +1,47 @@
+
+//1-masala
+// console.log(parseInt(100,3));
+
+// 2-masala
+// ar=[1,2,3,4,'besh',6]
+// arr=ar.slice(0,3)
+// console.log(arr.splice(1,2));
+
+// 3-masala
+// stningni ortasiga string qanday qoshiladi
+
+
+// 4-masala
+          
+// arrow funksiya yasalishi qanday
+
+
+// 5-masala
+
+// fill nechtaparametr oladi va vazifasi
+
+// var Ar = [ 1,2,3 ,4,'besh',6]
+// var Arr = Ar.slice(0,3)
+// console.log(Arr.splice(1,2));
+// a= 122.1231
+// console.log((a).toFixed(-1));
+// for (let i = 0; i <-10; i--) {
+//     // const element = array[i];
+    
+//     console.log(i);
+// }
+// a=[10,100]
+// console.log((Array.from(a,x=> x+x)));
+
+// havo='njfa'
+// switch (havo) {
+//     case havo: 'nijfa'
+//         console.log(efweaf);
+//         break;
+
+//     default:
+//         break;
+// }
+// var a=12.5
+// console.log(a.toFixed(0));
+

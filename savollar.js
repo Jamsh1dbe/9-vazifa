333a87f829c7d0a3462ad85eeece346b77e960d2
JamShidBek2003 <92529675+Jamsh1dbe@users.noreply.github.com>
2021-11-20 22:29:56 +0500

    tets

--- savollar.js
+++ savollar.js
@@ -45,3 +45,98 @@
 // var a=12.5
 // console.log(a.toFixed(0));
 
+
+
+
+// con
+
+
+// const obj1={
+//     a: 1,
+//     b: 2,
+//     getA(){
+//         // return this.a
+//         console.log(this.a);
+//     },
+//     getB(){
+//         // return this.b
+//         console.log(this.b);
+//     }
+// }
+// obj1.getA().getB();
+// // console.log(obj1);
+
+
+
+// ar =[1,2,33,434,5,5]
+// console.log(ar.splice(2));
+
+
+
+// a ='jamshide jma sdjk'
+// console.log(a.replace('jma','jamshidbek'));
+
+// a ='877'
+// console.log(a.padEnd(6,7));
+
+// a='jam jamjam jam jam jam jam'
+// console.log(a.replace(/ja/gi,'kk'));
+
+
+// ar = 131314
+// console.log(ar.revorse());
+
+// asra='121,23px'
+
+// console.log(parseInt(asra));
+// console.log(parseFloat(asra));
+
+
+
+// obj =(num1=0,num2=0)=>{
+//     if(num2){console.log(num1+num2);
+//         return function (num3=0) {
+//             console.log(num1+num3);
+//         }
+//     }
+// }
+// obj(1, 2);
+// obj(1)(2);
+
+
+
+
+// for (let i = 1; i < 10; i++) {
+//     // console.log(i);
+//     for (let j = 1; j < i; j++) {
+//         console.log('i*j=', i*j);    
+        
+//     }    
+// }
+
+
+// var a='123'
+// sr='';
+// for (let i = a.length-1; i>=0; i--) {
+//     // sr+=a[i]
+//     // i
+//     // console.log(i);
+// } 
+
+
+// dj='dwre','ewrwe'
+// console.log(dj.reverse());
+
+
+// ar =['@','@','@','@',]
+// console.log(ar.every());
+// a=+('122.12'+4)
+// console.log(a);
+
+
+const tets=()=>{
+    console.log('test1');
+    return "test2"
+}
+tets()
+// console.log(tets());
